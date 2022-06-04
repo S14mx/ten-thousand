8610bb0dc66c670da51fd515f295570328fb3010
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-06-04 16:56:46 -0700

    Passed Six Tests

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,11 +1,17 @@
 import random
 
+
 class GameLogic:
 
   def __init__(self,data):
     self.data = data
+    self.next = next
   
   @staticmethod
-  def roll_dice():
-    return(random.randrange(0,6))
+  def roll_dice(value):
+    results =[]
+    for num in range(value):
+      result = (random.randrange(1,6))
+      results.append(result)
+    return results
     
