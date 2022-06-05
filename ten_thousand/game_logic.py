bc81a4a4e422cce51fc4a6a99da54bba96eeaf0e
S14mx <sergey.otryshko@gmail.com>
2022-06-05 16:35:33 -0700

    implemented calculate_score functionality

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,83 +1,40 @@
 import random
 from collections import Counter
 
+
 class GameLogic:
 
-  def __init__(self,data):
-    self.data = data
-    self.next = next
-  
-  @staticmethod
-  def roll_dice(value):
-    results =[]
-    for num in range(value):
-      result = (random.randrange(1,6))
-      results.append(result)
-    return tuple(results)
+    dice_value = {
+        1: {"single": 100, "multiple": 1000},
+        2: {"single": 0, "multiple": 200},
+        3: {"single": 0, "multiple": 300},
+        4: {"single": 0, "multiple": 400},
+        5: {"single": 50, "multiple": 500},
+        6: {"single": 0, "multiple": 600}
+    }
 
-  @staticmethod
-  def calculate_score(roll):
-    roll_points = Counter(roll)
-    if roll_points == Counter({1: 1}):
-      return 100
-    elif roll_points == Counter({1: 2}):
-      return 200
-    elif roll_points == Counter({1: 3}):
-      return 1000
-    elif roll_points == Counter({1: 4}):
-      return 2000
-    elif roll_points == Counter({1: 5}):
-      return 4000
-    elif roll_points == Counter({2: 3}):
-      return 200
-    elif roll_points == Counter({2: 4}):
-      return 400
-    elif roll_points == Counter({2: 5}):
-      return 600
-    elif roll_points == Counter({2: 6}):
-      return 800
-    elif roll_points == Counter({3: 3}):
-      return 300
-    elif roll_points == Counter({3: 4}):
-      return 600
-    elif roll_points == Counter({3: 5}):
-      return 900
-    elif roll_points == Counter({3: 6}):
-      return 1200
-    elif roll_points == Counter({4: 3}):
-      return 400
-    elif roll_points == Counter({4: 4}):
-      return 800
-    elif roll_points == Counter({4: 5}):
-      return 1200
-    elif roll_points == Counter({4: 6}):
-      return 1600
-    elif roll_points == Counter({5: 1}):
-      return 50
-    elif roll_points == Counter({5: 2}):
-      return 100
-    elif roll_points == Counter({5: 3}):
-      return 500
-    elif roll_points == Counter({5: 4}):
-      return 1000
-    elif roll_points == Counter({5: 5}):
-      return 1500
-    elif roll_points == Counter({5: 6}):
-      return 2000
-    elif roll_points == Counter({6: 3}):
-      return 600
-    elif roll_points == Counter({6: 4}):
-      return 1200
-    elif roll_points == Counter({6: 5}):
-      return 1800
-    elif roll_points == Counter({6: 6}):
-      return 2400
-    else:
-      return 0
+    @staticmethod
+    def roll_dice(value):
+        results = []
+        for num in range(value):
+            result = (random.randrange(1, 6))
+            results.append(result)
+        return tuple(results)
 
+    @staticmethod
+    def calculate_score(roll):
+        roll_points = Counter(roll)
+        score = 0
 
+        # Handle 3 pairs and straight
+        if (len(roll_points) == 3 and list(roll_points.values()) == [2, 2, 2]) or (len(roll_points) == 6):
+            return 1500
 
+        for dice in roll_points:
+            number_of_occurrences = roll_points[dice]
+            if number_of_occurrences < 3:
+                score += GameLogic.dice_value[dice]["single"] * number_of_occurrences
+            else:
+                score += GameLogic.dice_value[dice]["multiple"] * (number_of_occurrences - 2)
 
-
-
-    
+        return score
