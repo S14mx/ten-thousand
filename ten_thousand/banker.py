a7dafbe5fbef006d58be2cbb94dcf61ec6caa6d7
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-06-06 18:00:57 -0700

    Banker_Test_passed_README_update

--- ten_thousand/banker.py
+++ ten_thousand/banker.py
@@ -1,5 +1,13 @@
 
 
 class Banker:
-    def __init__(self,data):
-        self.data = data
+    def __init__(self, balance=0, shelved=0):
+        self.balance = balance
+        self.shelved = shelved
+    def shelf(self, points):
+        self.shelved = points
+    def bank(self):
+        self.balance = self.shelved
+        self.clear_shelf()
+    def clear_shelf(self):
+        self.shelved = 0
--- tests/version_1/test_banker.py
+++ tests/version_1/test_banker.py
@@ -3,20 +3,20 @@ from ten_thousand.banker import Banker
 
 pytestmark = [pytest.mark.version_1]
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_new_banker():
     banker = Banker()
     assert banker.balance == 0
     assert banker.shelved == 0
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_shelf():
     banker = Banker()
     banker.shelf(100)
     assert banker.shelved == 100
     assert banker.balance == 0
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_deposit():
     banker = Banker()
     banker.shelf(100)
@@ -24,7 +24,7 @@ def test_deposit():
     assert banker.shelved == 0
     assert banker.balance == 100
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_clear_shelf():
     banker = Banker()
     banker.shelf(100)
