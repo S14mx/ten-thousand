2760df6e5fa8725381e16047e2c77dbb7fea716d
S14mx <sergey.otryshko@gmail.com>
2022-06-10 17:08:44 -0700

    started code refactoring

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,13 +1,30 @@
-from unicodedata import name
+
 from ten_thousand.game_logic import GameLogic
 from ten_thousand.banker import Banker
 
 
-class Game:
-    def __init__(self):
-        pass
+class EndOfGame(Exception):
+    pass
 
-    def handle_roll(self, roller=GameLogic.roll_dice):
+
+class Game:
+    def __init__(self, round_counter=1):
+        self.round_counter = round_counter
+
+    def __capture_input(self, text):
+        user_response = input(text)
+        if user_response == "q":
+            raise EndOfGame
+        return user_response
+
+    def handle_shelf(self, answer, banker, calculate, roll):
+        answer_to_int = tuple(int(dice) for dice in answer)
+        calc_points = calculate(answer_to_int)
+        banker.shelf(calc_points)
+        print(f"You have {banker.shelved} unbanked points and {len(roll) - len(answer_to_int)} dice remaining")
+        print("(r)oll again, (b)ank your points or (q)uit:")
+
+    def handle_roll(self, roller):
         roll = roller(6)
         roll_str = ""
         for dice in roll:
@@ -15,76 +32,49 @@ class Game:
         print(f"*** {roll_str}***")
         print("Enter dice to keep, or (q)uit:")
 
+    def handle_bank(self, banker, round_counter):
+        print(f"You banked {banker.shelved} points in round {self.round_counter}")
+        banker.bank()
+        self.round_counter += 1
+        print(f"Total score is {banker.balance} points")
+
     def play(self, roller=GameLogic.roll_dice, calculate=GameLogic.calculate_score, banker=Banker()):
-        round_counter = 1
-        print("Welcome to Ten Thousand")
-        print("(y)es to play or (n)o to decline")
-        answer = input("> ")
-        if answer == 'n':
-            print("OK. Maybe another time")
-        else:
-            print(f"Starting round {round_counter}")
-            print("Rolling 6 dice...")
-            roll = roller(6)
-            roll_str = ""
-            for dice in roll:
-                roll_str += str(dice) + " "
-            print(f"*** {roll_str}***")
-            print("Enter dice to keep, or (q)uit:")
-            answer = input("> ")
-
-            if answer == "q":
-                print("Thanks for playing. You earned 0 points")
-
+        try:
+            print("Welcome to Ten Thousand")
+            print("(y)es to play or (n)o to decline")
+            answer = self.__capture_input("> ")
+            if answer == 'n':
+                print("OK. Maybe another time")
             else:
-                answer_to_int = tuple(int(dice) for dice in answer)
-                calc_points = calculate(answer_to_int)
-                banker.shelf(calc_points)
-                print(f"You have {banker.shelved} unbanked points and {len(roll) - len(answer_to_int)} dice remaining")
-                print("(r)oll again, (b)ank your points or (q)uit:")
-
-                answer = input("> ")
+                print(f"Starting round {self.round_counter}")
+                print("Rolling 6 dice...")
+                roll = roller(6)
+                roll_str = ""
+                for dice in roll:
+                    roll_str += str(dice) + " "
+                print(f"*** {roll_str}***")
+                print("Enter dice to keep, or (q)uit:")
+                answer = self.__capture_input("> ")
+                self.handle_shelf(answer, banker, calculate, roll)
+                answer = self.__capture_input("> ")
                 if answer == "b":
-                    print(f"You banked {banker.shelved} points in round {round_counter}")
-                    banker.bank()
-                    round_counter += 1
-                    print(f"Total score is {banker.balance} points")
-                    print(f"Starting round {round_counter}")
+                    self.handle_bank(banker, self.round_counter)
+                    print(f"Starting round {self.round_counter}")
                     print("Rolling 6 dice...")
-                    roll = roller(6)
-                    roll_str = ""
-                    for dice in roll:
-                        roll_str += str(dice) + " "
-                    print(f"*** {roll_str}***")
-                    print("Enter dice to keep, or (q)uit:")
-                    answer = input("> ")
-                    if answer == "q":
-                        print(f"Thanks for playing. You earned {banker.balance} points")
-                        banker.clear_balance()
-                    else:
-                        answer_to_int = tuple(int(dice) for dice in answer)
-                        calc_points = calculate(answer_to_int)
-                        banker.shelf(calc_points)
-                        print(f"You have {banker.shelved} unbanked points and {len(roll) - len(answer_to_int)} dice remaining")
-                        print("(r)oll again, (b)ank your points or (q)uit:")
-                        answer = input("> ")
-                        if answer == "b":
-                            print(f"You banked {banker.shelved} points in round {round_counter}")
-                            banker.bank()
-                            round_counter += 1
-                            print(f"Total score is {banker.balance} points")
-                            print(f"Starting round {round_counter}")
-                            print("Rolling 6 dice...")
-                            roll = roller(6)
-                            roll_str = ""
-                            for dice in roll:
-                                roll_str += str(dice) + " "
-                            print(f"*** {roll_str}***")
-                            print("Enter dice to keep, or (q)uit:")
-                            answer = input("> ")
-                            if answer == "q":
-                                print(f"Thanks for playing. You earned {banker.balance} points")
-                                banker.clear_balance()
+                    self.handle_roll(roller)
+                    answer = self.__capture_input("> ")
+                    self.handle_shelf(answer, banker, calculate, roll)
+                    answer = self.__capture_input("> ")
+                    if answer == "b":
+                        self.handle_bank(banker, self.round_counter)
+                        print(f"Starting round {self.round_counter}")
+                        print("Rolling 6 dice...")
+                        self.handle_roll(roller)
+                        answer = self.__capture_input("> ")
+
+        except EndOfGame:
+            print(f"Thanks for playing. You earned {banker.balance} points")
+            banker.clear_balance()
 
 
 if __name__ == "__main__":
